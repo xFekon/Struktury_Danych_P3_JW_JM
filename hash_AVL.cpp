010c279953be3bbbafa097982b564ecaeac6644e
xFekon <272610@student.pwr.edu.pl>
2025-06-03 22:38:45 +0200

    hashowanie AVL

--- hash_AVL.cpp
+++ hash_AVL.cpp
@@ -1,1 +1,36 @@
 #include "hash_AVL.h"
+
+int hash_AVL::hash(int key) const {
+    return key % capacity; // Mo¿na u¿yæ lepszej, ale to wystarczy
+}
+
+// Konstruktor
+hash_AVL::hash_AVL(int size)
+    : capacity(size), table(size, nullptr) {
+    for (int i = 0; i < capacity; ++i)
+        table[i] = new AVL_tree();
+}
+
+// Destruktor
+hash_AVL::~hash_AVL() {
+    for (int i = 0; i < capacity; ++i)
+        delete table[i];
+}
+
+// Wstawianie
+void hash_AVL::insert(int key, int value) {
+    int index = hash(key);
+    table[index]->insert(key, value);
+}
+
+// Usuwanie
+void hash_AVL::remove(int key) {
+    int index = hash(key);
+    table[index]->remove(key);
+}
+
+// Szukanie
+bool hash_AVL::find(int key, int& value) {
+    int index = hash(key);
+    return table[index]->find(key, value);
+}
--- hash_AVL.h
+++ hash_AVL.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <vector>
+#include "AVL_tree.h"
 class hash_AVL
 {
+private:
+    std::vector<AVL_tree*> table;
+    int capacity;
+
+    int hash(int key) const;
+
+public:
+    hash_AVL(int size = 101); // domyœlna wielkoœæ tablicy
+    ~hash_AVL();
+
+    void insert(int key, int value);
+    void remove(int key);
+    bool find(int key, int& value);
 };
 
